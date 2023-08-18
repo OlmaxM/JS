9019f6ea35870dd6ff8c34f87283b693d542c005
User <ol.maksymiuk@gmail.com>
2023-08-19 01:16:25 +0300

    Initial commit

---
+++ Lesson1HW/Js.js
@@ -0,0 +1,29 @@
+// console.log('hello')
+// console.log('owu')
+// console.log('com')
+// console.log('ua')
+// console.log(1)
+// console.log(10)
+// console.log(-999)
+// console.log(123)
+// console.log( 3.14)
+// console.log(2.7,)
+// console.log(16)
+// console.log(true)
+// console.log(false)
+// console.log(16)
+// let firstName = 'Oleg';
+// let middleName = 'Maksymiuk';
+// let lastName = 'Ivanovich';
+// let Person = `${ firstName }` + `${ middleName }` + `${ lastName }`;
+// console.log(Person);
+let a = 100; let b = '100'; let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+let q= prompt('firstName')
+let w =prompt('middleName')
+let e=prompt('lastName')
+let r = q+w+e;
+console.log( r )
+
